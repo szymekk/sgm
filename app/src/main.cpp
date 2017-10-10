4b591e308d730caa8570ec3f59c2ad0ec29f18d0
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-10-10 23:27:26 +0200

    Add Grid struct
    The Grid struct encapsulates a two-dimensional array of objects of
    the same type.

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <cstdint>
+
+#include "img/core.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    using ImageRGB = img::ImageRGB<std::uint8_t>;
+    ImageRGB img_8bit(3, 2);
+    img_8bit.data.front() = {'b','e','g'};
+    img_8bit.data[1] = {'a','b','c'};
+    img_8bit.data.back() = {'e','n','d'};
+
+    for(size_t r = 0; r < img_8bit.height; ++r) {
+        std::cout << r << ":\t";
+        for(size_t c = 0; c < img_8bit.width; ++c) {
+            const auto& cell = img_8bit.get(c, r);
+            std::cout << "{";
+            std::cout << cell.red   << ", ";
+            std::cout << cell.green << ", ";
+            std::cout << cell.blue  << "}\t";
+        }
+        std::cout << "\n";
+    }
+
     return 0;
 }
-
--- img/include/img/core.h
+++ img/include/img/core.h
@@ -1,7 +1,45 @@
 #ifndef IMG_CORE_H
 #define IMG_CORE_H
 
+#include <cassert>
+#include <vector>
+
 namespace img {
+
+    template <typename PixelT>
+    struct Grid {
+        Grid(const std::size_t cols, const std::size_t rows)
+                : width(cols), height(rows), data(width*height)
+        {}
+
+        PixelT get(const std::size_t col, const std::size_t row) {
+            assert(col < width);
+            assert(row < height);
+            return data.at(col + width*row);
+        }
+
+        std::size_t width;
+        std::size_t height;
+        std::vector<PixelT> data;
+    };
+
+    template <typename ChannelT>
+    struct PixelGrey {
+        ChannelT value;
+    };
+
+    template <typename ChannelT>
+    struct PixelRGB {
+        ChannelT red;
+        ChannelT green;
+        ChannelT blue;
+    };
+
+    template <typename ChannelT>
+    using ImageGrey = Grid<PixelGrey<ChannelT>>;
+
+    template <typename ChannelT>
+    using ImageRGB = Grid<PixelRGB<ChannelT>>;
 
 } // namespace img
 
