3fcc74f3a2841fb978bc2a7da541d144b6e12515
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-10-12 00:08:18 +0200

    Fix indenation

--- img/include/img/core.h
+++ img/include/img/core.h
@@ -6,40 +6,40 @@
 
 namespace img {
 
-    template <typename PixelT>
-    struct Grid {
-        Grid(const std::size_t cols, const std::size_t rows)
-                : width(cols), height(rows), data(width*height)
-        {}
+template <typename PixelT>
+struct Grid {
+    Grid(const std::size_t cols, const std::size_t rows)
+            : width(cols), height(rows), data(width*height)
+    {}
 
-        PixelT get(const std::size_t col, const std::size_t row) {
-            assert(col < width);
-            assert(row < height);
-            return data.at(col + width*row);
-        }
+    PixelT get(const std::size_t col, const std::size_t row) {
+        assert(col < width);
+        assert(row < height);
+        return data.at(col + width*row);
+    }
 
-        std::size_t width;
-        std::size_t height;
-        std::vector<PixelT> data;
-    };
+    std::size_t width;
+    std::size_t height;
+    std::vector<PixelT> data;
+};
 
-    template <typename ChannelT>
-    struct PixelGrey {
-        ChannelT value;
-    };
+template <typename ChannelT>
+struct PixelGrey {
+    ChannelT value;
+};
 
-    template <typename ChannelT>
-    struct PixelRGB {
-        ChannelT red;
-        ChannelT green;
-        ChannelT blue;
-    };
+template <typename ChannelT>
+struct PixelRGB {
+    ChannelT red;
+    ChannelT green;
+    ChannelT blue;
+};
 
-    template <typename ChannelT>
-    using ImageGrey = Grid<PixelGrey<ChannelT>>;
+template <typename ChannelT>
+using ImageGrey = Grid<PixelGrey<ChannelT>>;
 
-    template <typename ChannelT>
-    using ImageRGB = Grid<PixelRGB<ChannelT>>;
+template <typename ChannelT>
+using ImageRGB = Grid<PixelRGB<ChannelT>>;
 
 } // namespace img
 
