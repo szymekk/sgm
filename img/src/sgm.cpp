10d72a5927db0fc17a5191ae2ed1fa1a5cf1e5a0
Szymon Kozłowski <szymek.kozlowski1@gmail.com>
2017-10-14 20:52:51 +0200

    Add window-based sum of absolute differences cost function.

--- img/src/sgm.cpp
+++ img/src/sgm.cpp
@@ -49,6 +49,80 @@ calculate_costs(const img::ImageGray<std::uint8_t>& left, const img::ImageGray<s
     }
     return costs;
 }
+[[maybe_unused]]
+cost_value_t pixelwise_absolute_difference(
+        const img::ImageGray<std::uint8_t>& left,
+        const img::ImageGray<std::uint8_t>& right,
+        const std::size_t row,
+        const std::size_t col,
+        const std::size_t disparity) {
+    if (col < disparity) {
+        return INVALID_COST;
+    } else {
+        return abs_diff(left.get(col, row).value, right.get(col-disparity, row).value);
+    }
+}
+
+cost_value_t sum_of_absolute_differences(
+        const img::ImageGray<std::uint8_t>& left,
+        const img::ImageGray<std::uint8_t>& right,
+        const std::size_t row,
+        const std::size_t col,
+        const std::size_t disparity,
+        const std::size_t window_size) {
+    assert(window_size > 0);
+
+    const std::size_t n = (window_size - 1) / 2;
+    unsigned int sum = 0;
+    std::size_t valid_pixels = 0;
+
+    assert(
+            std::numeric_limits<decltype(sum)>::max() >=
+            window_size * window_size * std::numeric_limits<cost_value_t>::max());
+
+
+    const std::size_t r_min = (row >= n) ? row - n : 0;
+    const std::size_t r_max = std::clamp(row + n, 0uL, left.height - 1);
+
+    const std::size_t c_min = (col >= n) ? col - n : 0;
+    const std::size_t c_max = std::clamp(col + n, 0uL, left.width - 1);
+
+    // std::cerr << "r(" << r_min << "-" << r_max << "), c(" << c_min << "-" << c_max << ")\n";
+
+    for (std::size_t r = r_min; r <= r_max; ++r) {
+        for (std::size_t c = c_min; c <= c_max; ++c) {
+            if (r < left.height && c < left.width && c >= disparity) { // r, c always >= 0 (unsigned)
+                ++valid_pixels;
+                sum += abs_diff(left.get(c, r).value, right.get(c - disparity, r).value);
+            } else {
+                // std::cerr << "invalid pixel: "
+                // << "row: " << row
+                // << ", col: " << col
+                // << ", disparity: " << disparity
+                // << "\n";
+            }
+        }
+    }
+    if (0 == valid_pixels) {
+        std::cerr
+            << "row: " << row
+            << ", col: " << col
+            << ", disparity: " << disparity
+            << "\n";
+    }
+    assert(valid_pixels > 0);
+    // std::cerr << "sum: " << sum << ", valid_pixels: " << valid_pixels << ", result: " << sum / valid_pixels << "\n";
+    return static_cast<cost_value_t>(sum / valid_pixels);
+}
+
+cost_value_t sum_of_absolute_differences_3(
+        const img::ImageGray<std::uint8_t>& left,
+        const img::ImageGray<std::uint8_t>& right,
+        const std::size_t row,
+        const std::size_t col,
+        const std::size_t disparity) {
+    return sum_of_absolute_differences(left, right, row, col, disparity, 3);
+}
 
 } // namespace
 
